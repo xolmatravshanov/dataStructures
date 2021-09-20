7ebf656594c064267bc58a3c0a0ed3a28f8bde66
Xolmat <xolmatrw@gmail.com>
2021-09-20 14:37:17 +0500

    shift method was added to doubly linked list

--- double_linked_list/index.js
+++ double_linked_list/index.js
@@ -49,6 +49,18 @@ class DoublyLinkedList {
         return popedNode;
     }
 
- 
+    shift(){
+        if(this.length === 0)return undefined;
+        var oldHead = this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }
+        this.head = oldHead.next;
+        this.head.prev = null;
+        oldHead.next = null;
+        this.length--;
+        return oldHead;
+    }
 
 }
