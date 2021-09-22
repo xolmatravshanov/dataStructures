fa5959cd9a25caa879aea166b019241f2d56ab04
Xolmat <xolmatrw@gmail.com>
2021-09-22 11:24:20 +0500

    sorting algoritim was added

--- graph/graph.js
+++ graph/graph.js
@@ -1,14 +1,18 @@
 class Graph{
+
     constructor(){
         this.adjacencyList = {};
     }
+
     addVertex( vertex ){
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+
     addEdge(v1,v2){
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
+
     removeEdge(vertex1,vertex2){
         this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
             v => v !== vertex2
@@ -17,6 +21,7 @@ class Graph{
             v => v !== vertex1
         );
     }
+    
     removeVertex(vertex){
         while(this.adjacencyList[vertex].length){
             const adjacentVertex = this.adjacencyList[vertex].pop();
@@ -41,6 +46,4 @@ g.addEdge("Los Angeles", "Aspen");
 
 
 
-
-
-
+console.log(g);
